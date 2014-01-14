ec810dfb02bd61be7cf1d9ace33abd5b353b2be6
John Kropka <john@cabforward.com>
2014-01-14 17:59:13 -0600

    initial push of web services android to github

---
+++ gen/com/example/androidwsaccess/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.androidwsaccess;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/androidwsaccess/FullscreenActivity.java
@@ -0,0 +1,324 @@
+package com.example.androidwsaccess;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+ 
+import java.io.ByteArrayOutputStream;
+import com.example.androidwsaccess.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.net.NetworkInfo.State;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+//import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+//import android.app.Activity;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ * 
+ * @see SystemUiHider
+ */
+public class FullscreenActivity extends Activity {
+	
+	EditText etResponse;
+	/**
+	 * Whether or not the system UI should be auto-hidden after
+	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+	 */
+	private static final boolean AUTO_HIDE = true;
+
+	/**
+	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+	 * user interaction before hiding the system UI.
+	 */
+	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+	/**
+	 * If set, will toggle the system UI visibility upon interaction. Otherwise,
+	 * will show the system UI visibility upon interaction.
+	 */
+	private static final boolean TOGGLE_ON_CLICK = true;
+
+	/**
+	 * The flags to pass to {@link SystemUiHider#getInstance}.
+	 */
+	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+
+	/**
+	 * The instance of the {@link SystemUiHider} for this activity.
+	 */
+	private SystemUiHider mSystemUiHider;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_fullscreen);
+
+		final View controlsView = findViewById(R.id.fullscreen_content_controls);
+		final View contentView = findViewById(R.id.fullscreen_content);
+		
+		// Set up an instance of SystemUiHider to control the system UI for
+		// this activity.
+		mSystemUiHider = SystemUiHider.getInstance(this, contentView,
+				HIDER_FLAGS);
+		mSystemUiHider.setup();
+		mSystemUiHider
+				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
+					// Cached values.
+					int mControlsHeight;
+					int mShortAnimTime;
+
+					@Override
+					@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+					public void onVisibilityChange(boolean visible) {
+						if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+							// If the ViewPropertyAnimator API is available
+							// (Honeycomb MR2 and later), use it to animate the
+							// in-layout UI controls at the bottom of the
+							// screen.
+							if (mControlsHeight == 0) {
+								mControlsHeight = controlsView.getHeight();
+							}
+							if (mShortAnimTime == 0) {
+								mShortAnimTime = getResources().getInteger(
+										android.R.integer.config_shortAnimTime);
+							}
+							controlsView
+									.animate()
+									.translationY(visible ? 0 : mControlsHeight)
+									.setDuration(mShortAnimTime);
+						} else {
+							// If the ViewPropertyAnimator APIs aren't
+							// available, simply show or hide the in-layout UI
+							// controls.
+							controlsView.setVisibility(visible ? View.VISIBLE
+									: View.GONE);
+						}
+
+						if (visible && AUTO_HIDE) {
+							// Schedule a hide().
+							delayedHide(AUTO_HIDE_DELAY_MILLIS);
+						}
+					}
+				});
+
+		// Set up the user interaction to manually show or hide the system UI.
+		contentView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (TOGGLE_ON_CLICK) {
+					mSystemUiHider.toggle();
+				} else {
+					mSystemUiHider.show();
+				}
+			}
+		});
+
+		// Upon interacting with UI controls, delay any scheduled hide()
+		// operations to prevent the jarring behavior of controls going away
+		// while interacting with the UI.
+		findViewById(R.id.dummy_button).setOnTouchListener(
+				mDelayHideTouchListener);
+	}
+
+	
+	
+	// This method is called at button click because we assigned the name to the
+    // "OnClick property" of the button
+    public void wsClickHandler(View view) {
+	        
+	        
+	        // get reference to the views
+	        etResponse = (EditText) findViewById(R.id.editText1);
+	        
+	        //make the asynch call to the server
+	        new HttpAsyncTask().execute("http://33.33.33.100:3000/0.1/api_users.xml");
+ 
+        return;
+		}
+		
+		 private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+		        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+		        String line = "";
+		        String result = "";
+		        while((line = bufferedReader.readLine()) != null)
+		            result += line;
+		 
+		        inputStream.close();
+		        return result;
+		 
+		    }
+	
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+
+		// Trigger the initial hide() shortly after the activity has been
+		// created, to briefly hint to the user that UI controls
+		// are available.
+		delayedHide(100);
+	}
+
+	/**
+	 * Touch listener to use for in-layout UI controls to delay hiding the
+	 * system UI. This is to prevent the jarring behavior of controls going away
+	 * while interacting with activity UI.
+	 */
+	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+		@Override
+		public boolean onTouch(View view, MotionEvent motionEvent) {
+			if (AUTO_HIDE) {
+				delayedHide(AUTO_HIDE_DELAY_MILLIS);
+			}
+			return false;
+		}
+	};
+
+	Handler mHideHandler = new Handler();
+	Runnable mHideRunnable = new Runnable() {
+		@Override
+		public void run() {
+			mSystemUiHider.hide();
+		}
+	};
+
+	/**
+	 * Schedules a call to hide() in [delay] milliseconds, canceling any
+	 * previously scheduled calls.
+	 */
+	private void delayedHide(int delayMillis) {
+		mHideHandler.removeCallbacks(mHideRunnable);
+		mHideHandler.postDelayed(mHideRunnable, delayMillis);
+	}
+	
+	 public static String GET(String url){
+	        InputStream inputStream = null;
+	        String result = "";
+	        try {
+	 
+	            // create HttpClient
+	            HttpClient httpclient = new DefaultHttpClient();
+	 
+	            // make GET request to the given URLprivate
+	            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+	 
+	            // receive response as inputStream
+	            inputStream = httpResponse.getEntity().getContent();
+	 
+	            // convert inputstream to string
+	            if(inputStream != null)
+	                result = convertInputStreamToString(inputStream);
+	            else
+	                result = "Did not work!";
+	 
+	        } catch (Exception e) {
+	            Log.d("InputStream", e.getLocalizedMessage());
+	        }
+	 
+	        return result;
+	    }
+	 
+ 
+	
+	private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            return GET(urls[0]);
+        }
+        
+        
+        protected String getTextNodeValue(Node node) {
+            Node child;
+              if (node != null) {
+                if (node.hasChildNodes()) {
+                    child = node.getFirstChild();
+                    while(child != null) {
+                        if (child.getNodeType() == Node.TEXT_NODE) {
+                            return child.getNodeValue();
+                        }
+                        child = child.getNextSibling();
+                    }
+                }
+              }
+              return "";
+            
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            //etResByteArrayInputStreamponse.setText(result);rsers.DocumentBuilder;
+            try{
+              InputStream is = new ByteArrayInputStream(result.getBytes("UTF-8"));
+              DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+              DocumentBuilder builder = builderFactory.newDocumentBuilder();
+                Document document = builder.parse(is);
+                NodeList nodes = document.getElementsByTagName("email");
+                String output;
+                output = "";
+                for (int i = 0; i < nodes.getLength(); i++) {
+                	output +=  this.getTextNodeValue(nodes.item(i)) + "\n";
+                }
+                
+               
+                etResponse.setText(output);
+                
+             } catch (Exception e) {
+              	Log.d("builder", e.getLocalizedMessage());
+              	etResponse.setText("parsing error occured");
+              }
+              
+             
+            
+            
+           // String parsed_response = "";
+            
+           // //parse result which is XML to get the actual nodes in the response
+          //  DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+           // try{
+           //   DocumentBuilder builder = builderFactory.newDocumentBuilder();
+           //   Document document = builder.parse(result);
+           //   NodeList users = document.getElementsByTagName("strings");
+           //   etResponse.setText(parsed_response);
+           // } catch (Exception e) {
+	       //     Log.d("builderFactory", e.getLocalizedMessage());
+	        //    etResponse.setText("parsing error occured");
+	       // }
+            
+
+            
+           
+       }
+    }
+}
+
+
